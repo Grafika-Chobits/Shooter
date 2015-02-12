8687edf015426e0fcbeb4c6a6bfdb340d27101da
akhfa <akhmadfakhoni@gmail.com>
2015-02-12 12:22:42 +0700

    Tambah fungsi coloring. Belum fix

--- coloring.h
+++ coloring.h
@@ -4,20 +4,59 @@
 #include <stdio.h>
 #include "coordinate.h"
 #include "color.h"
+#include "frame.h"
 
 class Coloring{
 	private:
 		Coordinate upleft;
 		Coordinate downright;
+		
+		bool isSingularAtas(Frame * frm, Coordinate posisi)
+		{
+			int counter0 = 0;
+			Color color(0,0,0);
+			if(frm->getColor(posisi.getX()-1, 	posisi.getY()-1).isEqual(color))
+				counter0++;
+			if(frm->getColor(posisi.getX(), 	posisi.getY()-1).isEqual(color))
+				counter0++;
+			if(frm->getColor(posisi.getX()+1, 	posisi.getY()-1).isEqual(color))
+				counter0++;
+			return (counter0 == 1);
+		}
+			
+		bool isSingularBawah(Frame * frm, Coordinate posisi)
+		{
+			int counter0 = 0;
+			Color color(0,0,0);
+			if(frm->getColor(posisi.getX()-1, 	posisi.getY()+1).isEqual(color))
+				counter0++;
+			if(frm->getColor(posisi.getX(), 	posisi.getY()+1).isEqual(color))
+				counter0++;
+			if(frm->getColor(posisi.getX()+1, 	posisi.getY()+1).isEqual(color))
+				counter0++;
+			
+			return (counter0 == 1);
+		}
+		
+		bool isGarisDatar(Frame * frm, Coordinate posisi)
+		{
+			return frm->getColor(posisi.getX() + 1, posisi.getY()).isEqual(frm->getColor(posisi.getX(), posisi.getY()));
+		}
+		
+		bool isSingular(Frame * frm, Coordinate posisi)
+		{
+			return (isSingularAtas(frm, posisi) || isSingularBawah(frm, posisi));
+		}
+			
 	public:
 		Coloring(Coordinate _upleft, Coordinate _downright)
 		{
 			upleft 		= 	_upleft;
 			downright	=	_downright;
 		}
-		void drawColor(Color color)
+		void drawColor(Frame * frm, Color color) //
 		{
-			 
+			 //frm -> 
 		}
 };
 
